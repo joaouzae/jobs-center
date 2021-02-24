d51e27d5b617341cd20576a1f17b4b4c5cb94701
joaouzae <joao_uzae@hotmail.com>
2021-02-24 19:46:24 -0300

    antes de adicionar referencia de volta

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # employee1 = Employee.create!(email: 'joao@campuscode.com.br', password: '123456')
-l1 = Level.create!(name: "Júnior")
-l2 = Level.create!(name: "Pleno")
-l3 = Level.create!(name: "Sênior")
+

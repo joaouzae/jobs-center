f735a4fed38b2395e7a1fa23ea69244c20dfc3d9
joaouzae <joao_uzae@hotmail.com>
2021-02-25 23:49:07 -0300

    Cria funcionário atrelado à empresa

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -14,13 +14,29 @@ class CompaniesController < ApplicationController
   end
 
   def create
-    @company = Company.new(company_params())
+    @company = Company.last
+    @company.update(company_params())
 
     if @company.save
       redirect_to @company
       
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+  end
+  
+  def update
+    @company = Company.find(params[:id])
+    
+    
+    if @company.update(company_params())
+      redirect_to @company
+    else
+      render :edit
     end
   end
 
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,15 +8,20 @@ class Employee < ApplicationRecord
   validates :email, presence: true
   before_validation :create_company
   after_create :associate_domain_to_company
+
+  enum role: {member: 0, admin: 5}
+
   private
   def create_company
     #só criar uma nova empresa se não existir nenhuma com o mesmo domínio do employee
     company = Company.find_by(domain: self.employee_domain())
     if company
+      self.role = "member"
       self.company = company
     else
       self.company = Company.new
       self.company.save(:validate => false)
+      self.role = "admin"
     end
   end
 
